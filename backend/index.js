c9398d2d70f529331f0d0434dcf15df257165bf7
Andre <andre.peixoto@bestcontent.ai>
2024-10-21 23:03:27 -0300

    .

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import "dotenv/config"
 import app from "./src/config/app.js"
 
 const PORT = 8000;
-const HOST = "localhost";
+const HOST = "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
     console.log("Server listen on http://"+HOST+":"+PORT);
